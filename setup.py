6f9801e8c7cbf1f865feea41c16ac153dd152fa6
Rory Schadler <rory.h.schadler.21@dartmouth.edu>
2020-04-27 20:54:52 -0400

    restructured for packaging

--- setup.py
+++ setup.py
@@ -4,16 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="unit_converter_roryschadler",
-    version="0.1",
+    name="nanomine-unit-converter-roryschadler",
+    version="0.1.1dev",
     author="Rory Schadler",
+    author_email="rory.h.schadler.21@dartmouth.edu",
     description="Unit converter for NanoMine Knowledge Graph",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/roryschadler/nanomine_unit_converter",
-    packages=setuptools.find_packages(),
+    packages=['nanomine_unit_converter'],
+    include_package_data=True,
+    install_requires=['pint',
+                      'rdflib'],
     classifiers=[
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5.3'
+    python_requires='>=3.5.2'
 )
